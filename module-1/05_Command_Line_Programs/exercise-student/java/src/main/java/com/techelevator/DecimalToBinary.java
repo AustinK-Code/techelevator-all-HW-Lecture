c9c4dbe0c1f22998808a9a6952f9983be3551bb8
Austin Kinnard <austink145@gmail.com>
2021-01-17 21:08:45 -0500

    done except some some formatting issues I had

--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -13,7 +13,7 @@ public class DecimalToBinary {
 		//convert to in array
 		String[] inputArray = userInput.split(" ");
 
-		//create a for loop
+		//create a for loop to convert and print.... profit$$$$$$$
 		for (int i = 0 ; i < inputArray.length; i++){
 			int binaryIntConvert = Integer.parseInt(inputArray[i]);
 			System.out.println(inputArray[i] + " in binary is " + Integer.toBinaryString(binaryIntConvert));
