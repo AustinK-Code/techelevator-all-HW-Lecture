48050b4bd97d1903cb856a81e09d1eabebbc3238
David P-C <david.pc@techelevator.com>
2021-01-19 08:32:21 -0500

    Added lecture-student for m1-07.

---
+++ module-1/07_Collections_Part_1/lecture-student/java/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,77 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+
+	//1. returnsAnArray(5) -> {1, 2, 3, 4, 5}
+	public int[] returnsAnArray(int number) {
+		return null;
+	}
+
+	//2. returnsAList(5) -> [1, 2, 3, 4, 5]
+	public List<Integer> returnsAList(int number) {
+		return null;
+	}
+
+	//3. returnThirdName(["Frodo","Sam","Merry","Pippin"]) -> "Merry"
+	public String returnThirdName(List<String> names) {
+		return null;
+	}
+
+	//4. insertAfterFirst(["Luke","Leia"], "Han") -> ["Luke", "Han", "Leia"]
+	public List<String> insertAfterFirst(List<String> names, String name) {
+		return null;
+	}
+
+	//5. onlyOneAnswer(["yes", "no", "maybe"]) -> false
+	//   onlyOneAnswer(["no"]) -> true
+	public boolean onlyOneAnswer(List<String> answers) {
+		return false;
+	}
+
+	//6. removeLastNumber([1.5, 2.2, 0.9]) -> [1.5, 2.2]
+	public List<Double> removeLastNumber(List<Double> numbers) {
+		return null;
+	}
+
+	//7. hasACow(["duck","cow","chicken"]) -> true
+	//   hasACow(["turkey", "rooster"]) -> false
+	public boolean hasACow(List<String> farmAnimals) {
+		return false;
+	}
+
+	//8. yourPlaceInLine(["customer","customer","you","customer"]) -> 3
+	public int yourPlaceInLine(List<String> allCustomers) {
+		return -1;
+	}
+
+	//9. trimArray({"a","b","c","d"}) -> {"b","c"}
+	public String[] trimArray(String[] input) {
+		return null;
+	}
+
+	//10. descendingOrder([3, 4, 2, 9, 4, 8]) -> [9, 8, 4, 4, 3, 2]
+	public List<Integer> descendingOrder(List<Integer> numbers) {
+		return null;
+	}
+
+	//11. countTrues([false,true,true,false,true]) -> 3
+	public int countTrues(List<Boolean> answers) {
+		return -1;
+	}
+
+	//12. incrementAll([100, 17, 33]) -> [101, 18, 34]
+	public List<Integer> incrementAll(List<Integer> numbers) {
+		return null;
+	}
+
+	//13. echo(["Tech","Elevator"]) -> ["Tech","Tech","Elevator","Elevator"]
+	public List<String> echo(List<String> words) {
+		return null;
+	}
+
+}
