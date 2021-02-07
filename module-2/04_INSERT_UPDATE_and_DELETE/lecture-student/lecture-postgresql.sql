d80b9f2c018a0a7b3aaa94e332571b52a70d477b
katie dwyer <katie.dwyer@techelevator.com>
2021-02-07 16:14:09 -0500

    module 2 week 1 student lectures

---
+++ module-2/04_INSERT_UPDATE_and_DELETE/lecture-student/lecture-postgresql.sql
@@ -0,0 +1,51 @@
+-- INSERT
+
+-- 1. Add Klingon as a spoken language in the USA
+-- 2. Add Klingon as a spoken language in Great Britain (GBR)
+
+
+-- UPDATE
+
+-- 1. Update the capital of the USA to Houston
+-- 2. Update the capital of the USA to Washington DC and the head of state
+
+
+-- DELETE
+
+-- 1. Delete English as a spoken language in the USA
+-- 2. Delete all occurrences of the Klingon language 
+
+
+-- REFERENTIAL INTEGRITY
+
+-- 1. Try just adding Elvish to the country language table.
+
+-- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
+
+-- 3. Try deleting the country USA. What happens?
+
+
+-- CONSTRAINTS
+
+-- 1. Try inserting English as a spoken language in the USA
+
+-- 2. Try again. What happens?
+
+-- 3. Let's relocate the USA to the continent - 'Outer Space'
+
+
+-- How to view all of the constraints
+
+SELECT pg_constraint.*
+FROM pg_catalog.pg_constraint
+INNER JOIN pg_catalog.pg_class ON pg_class.oid = pg_constraint.conrelid
+INNER JOIN pg_catalog.pg_namespace ON pg_namespace.oid = connamespace;
+
+
+-- TRANSACTIONS
+
+-- 1. Try deleting all of the rows from the country language table and roll it back.
+
+-- 2. Try updating all of the cities to be in the USA and roll it back
+
+-- 3. Demonstrate two different SQL connections trying to access the same table where one happens to be inside of a transaction but hasn't committed yet.
